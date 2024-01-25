35f4d0d12e0cdd5a70d043cc36f4693f942dc7d0
Daiane Bolzan <dainha85@gmail.com>
2024-01-25 12:20:00 -0300

    fix: updater the name of table that need to be checked on database

--- src/middleware/checkLogin.js
+++ src/middleware/checkLogin.js
@@ -12,18 +12,19 @@ const checkLogin = async (req, res, next) => {
 
   try {
     const {id} = jwt.verify(token, process.env.JWT_PASSWORD);
-
+  
     const {rows, rowCount} = await pool.query(
-      "select * from user where id = $1",
+      "select * from users where id = $1",
       [id]
     );
+
     if (rowCount === 0) {
         
       return res.status(401).json({mensagem: "Para acessar este recurso um token de autenticação válido deve ser enviado."});
     }
     const {password, ...user} = rows[0];
-
     req.user = user;
+    
 
     next()
 
