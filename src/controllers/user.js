8e509c569e1760d95305f063dd7d4fb9711af27d
Daiane Bolzan <dainha85@gmail.com>
2024-01-25 12:00:27 -0300

    feat: add verifications to login and newUser

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,21 +1,69 @@
-const newUser = (req, res)=>{
-    const {nome, email, senha} = req.body;
-    if(!nome || !email || !senha){
-        return res.status(400).json({error: 'Dados incompletos'})
+const pool = require("../conexaodb");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const newUser = async (req, res)=>{
+    const {name, email, password} = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({mensagem: "Todos os campos são obrigatórios"});
+  }
+ 
+  try {
+    const emailExists = await pool.query(
+      "select * from users where email = $1",
+      [email]
+    );
+
+    if (emailExists.rowCount > 0) {
+      return res.status(400).json({
+        mensagem: "Já existe usuário cadastrado com o e-mail informado",
+      });
     }
-    if(senha.length < 6 ){
-        return res.status(400).json({error: 'Senha muito curta'})
-    }
-    
-    res.status(201).json({mensagem: 'Usuário cadastrado com sucesso'})
+    const encriptedPassword = await bcrypt.hash(password, 10);
+
+    const query =
+      "insert into users (name, email, password) values ($1, $2, $3) returning *";
+    const body = [name, email, encriptedPassword];
+    const {rows} = await pool.query(query, body);
+
+    const {password: _, ...user} = rows[0];
+
+    return res.status(201).json(user);
+  } catch (error) {
+    return res.status(500).json({mensagem: "Erro interno do servidor"});
+  }
 }
-const login = (req, res)=>{
-    const {email, senha} = req.body;
-    if(!email || !senha){
-        return res.status(400).json({error: 'Dados incompletos'})
-    }
-    res.status(200).json({email, senha})
+const login = async (req, res)=>{
+    const {email, password} = req.body;
+ 
+ 
+  try {
+    const {rows, rowCount} = await pool.query(
+      "select * from users where email = $1",
+      [email]
+    );
     
+
+    if (rowCount === 0) {
+      return res.status(400).json({mensagem: "Usuário e/ou senha inválido(s)"});
+    }
+    const {password: userPassword, ...user} = rows[0];
+
+    const correctPassword = await bcrypt.compare(password, userPassword);
+
+    if (!correctPassword) {
+      return res.status(400).json({mensagem: "Usuário e/ou senha inválido(s)"});
+    }
+    const token = jwt.sign({id: user.id}, process.env.JWT_PASSWORD, {expiresIn: "8h"});
+
+    return res.json({
+      user,
+      token,
+    });
+  } catch (error) {
+    console.error(error.message)
+    return res.status(500).json({mensagem: "Erro interno do servidor"});
+  }
 }
 
 module.exports = {newUser, login}
