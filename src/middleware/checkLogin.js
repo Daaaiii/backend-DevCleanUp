fba5fd22743910cfcc61ad85406d240a1ad8507a
Daiane Bolzan <dainha85@gmail.com>
2024-01-25 12:02:14 -0300

    feat: add middleware to verify if user is logged in

---
+++ src/middleware/checkLogin.js
@@ -0,0 +1,35 @@
+const pool = require("../conexaodb");
+const jwt = require("jsonwebtoken");
+
+
+const checkLogin = async (req, res, next) => {
+  const {authorization} = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({mensagem: "Para acessar este recurso um token de autenticação válido deve ser enviado."});
+  }
+  const token = authorization.split(" ")[1];
+
+  try {
+    const {id} = jwt.verify(token, process.env.JWT_PASSWORD);
+
+    const {rows, rowCount} = await pool.query(
+      "select * from user where id = $1",
+      [id]
+    );
+    if (rowCount === 0) {
+        
+      return res.status(401).json({mensagem: "Para acessar este recurso um token de autenticação válido deve ser enviado."});
+    }
+    const {password, ...user} = rows[0];
+
+    req.user = user;
+
+    next()
+
+  } catch (error) {
+  console.log(error.message)
+    return res.status(401).json({mensagem: "Para acessar este recurso um token de autenticação válido deve ser enviado."});
+  }
+};
+module.exports = checkLogin;
