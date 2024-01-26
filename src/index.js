b826093ed552da6c0fbf997afda5c3e6c40216be
Daiane Bolzan <dainha85@gmail.com>
2024-01-26 14:08:23 -0300

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require ('express')
+const cors = require('cors')
 const routes = require('./routes')
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger-output.json')
@@ -7,7 +8,8 @@ const swaggerFile = require('./swagger-output.json')
 const app = express()
 
 app.use(express.json())
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use(cors())
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(routes)
 
 const port = process.env.PORT || 3000
